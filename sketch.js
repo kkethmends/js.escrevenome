7cb6a8a1d79833fa27993dc7dc5e609695dd7e92
kkethmends <00001106684795sp@al.educacao.sp.gov.br>
2024-06-06 12:13:42 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(600, 600);
+    background("purple");
+  }
+  
+  function draw() {
+   
+    
+    stroke("pink")
+    fill("black")
+    
+   //  console.log(mouseIsPressed)
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY,20,35)}
+    
+  
+  }
